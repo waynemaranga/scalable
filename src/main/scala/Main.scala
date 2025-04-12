d1607717dde212cf3874f8d9181e00e49a6b34f9
WM <wmrngcd@gmail.com>
2025-04-12 22:09:18 +0300

    date time :)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 object Main extends App {
-  println("Hello, World!")
+
+  val currentTime = LocalDateTime.now()
+  val datetimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+  val formattedCurrentTime = currentTime.format(datetimeFormatter)
+
+  println(s"Hello 😄, its $formattedCurrentTime")
 }
